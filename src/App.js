14b64af8e899e9f6230173b19fc58aab0b73846e
SriGR <srigokulranjith1998@gmail.com>
2022-11-27 23:57:30 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {PieChart,Pie, Tooltip} from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend } from 'recharts';
 
-function App() {
+let App = () => {
+  let JSONdata= [
+      {name:"X",value:100},
+      {name:"Y",value:120},
+      {name:"Z",value:50}
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div> <h1>CHARTS :</h1>
+    <div  className="App">
+     <PieChart width={300} height={300}>
+     <Pie
+            dataKey="value" 
+            isAnimationActive={false}
+            data={JSONdata}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#6666ff"
+            label
+          />
+      <Tooltip/> 
+     </PieChart>
+     <BarChart
+          width={500}
+          height={300}
+          data={JSONdata}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="value" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="value" fill="green" />
+          <Bar dataKey="value" fill="blue" />
+        </BarChart>
+    </div>
     </div>
   );
 }
